1590a7a730c84fb882d72e3b2ab55e63378fd858
Max <max.krupen2003@gmail.com>
2022-04-26 15:13:27 +0300

    First commit

---
+++ Lab2/Factory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Factory : IFactory
+    {
+        public IFigure create(string figureName)
+        {
+            switch (figureName)
+            {
+                case "Отрезок":
+                    return new Line();
+                case "Прямоугольник":
+                    return new Rectangle();
+                case "Эллипс":
+                    return new Ellipse();
+                case "Ломанная":
+                    return new BrokenLine();
+                case "Многоугольник":
+                    return new Polygon();
+                default:
+                    return null;
+            }
+        }
+    }
+    interface IFactory
+    {
+        IFigure create(string figureName);
+    }
+}
---
+++ Lab2/Line.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Line : IFigure
+    {
+        public void Create()
+        {
+                
+        }
+        public override string ToString()
+        {
+            return "Линия";
+        }
+    }
+}
